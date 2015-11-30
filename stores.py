1bdffcf80281671ffab774cb9af3f21113f74562
Vivek Juneja <vivekjuneja@Viveks-MacBook-Pro.local>
2015-11-30 12:18:13 +0900

    Adding Redis support

--- stores.py
+++ stores.py
@@ -2,7 +2,9 @@ import collections
 import logging
 import logging.handlers
 import urlparse
-
+import redis
+import os
+from flask import jsonify
 
 class InMemoryStore(object):
 
@@ -11,12 +13,27 @@ class InMemoryStore(object):
         max_length = int(qa.get('max_length', ['100'])[0])
 
         print 'Configuring in-memory store with {settings}'.format(settings={"max_length": max_length})
-
         self.events = collections.deque(maxlen=max_length)
 
     def save(self, event):
-        print event
+        #print event
+ 	print 'saving event..... \n\n'
         self.events.append(event)
+	#Save to Redis
+	print 'opening connection to redis \n'
+	print 'checking deployment count \n'
+        val= os.popen("curl -s localhost:5000/events 2>/dev/null | jq '.events[] | select (.eventType == \"deployment_success\") | .eventType' | wc -l").read()
+        #cmdStr = "echo " + str(jsonResponse) + " | jq '.events[] | select (.eventType == \"deployment_success\") | .eventType' | wc -l"
+	#val = os.popen(cmdStr).read()
+	print 'val : ' + val
+	val1 = val.strip()
+	os.close()
+	r = redis.StrictRedis(host='192.168.99.100', port=6379, db=0)
+ 	#r.set("app-deployment-counter", int(val))	
+	print 'saving to redis \n'
+ 	r.set("app-deployment-counter", val1)	
+	print '\nsaved to redis\n'
+
 
     def list(self):
         return list(self.events)
@@ -44,3 +61,12 @@ class SyslogUdpStore(object):
 
     def list(self):
         return []
+
+
+
+if __name__ == '__main__':
+	event_store = InMemoryStore('http://localhost/events')
+	event_store.save(None)
+
+
+
